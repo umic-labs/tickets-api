009baec9cf0b2053c4876e1c6131171acda28e71
Igor Aragão <roginaldosemog@gmail.com>
2020-12-10 16:05:30 -0300

    Configura porta dinâmica para deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json())
+app.use(cors())
+app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3000);
