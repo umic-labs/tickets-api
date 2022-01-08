699e095f4539ae060bafb802a1a29072e8376295
Igor Aragão <roginaldosemog@gmail.com>
2022-01-08 18:46:21 -0300

    Adiciona abacate service

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const MailController = require("./controllers/MailController");
+const AbacateController = require("./services/abacate.service");
 
 const routes = express.Router();
 
 routes.post("/mail/new_order", MailController.new_order);
 routes.post("/mail/confirm_order", MailController.confirm_order);
+routes.post("/abacate", AbacateController.sendCreditCardCharge);
 
 module.exports = routes;
